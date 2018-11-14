490cc628a5cafaae7191c1b868db754b40c3f015
sebastjanst <sebastjan92@gmail.com>
2018-11-14 19:40:53 +0100

    fixed git/first 2 commits
    The github project was set up wrong so I made a fresh proper one just in case.
    Here's the first two commits:
    13.11.2018:
    Made groundwork for UI and some placeholder sprites.
    14.11.2018:
    -Implemented growing rose with watering function and collection function.
    -Side panels can now be opened by pressing their buttons and closed with esc.

---
+++ Assets/Scripts/GrowthScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrowthScript : MonoBehaviour {
+
+    private float WaterPercentage;//if 0 plant won't grow
+    private float PlantGrowth;//0 to 100, every 25 is a new stage (new sprite) the sprite at 100 shows the plant finished growing
+    public bool PlantDone;
+
+    private Image PlantSprite;
+    public Sprite PlantSprite1;
+    public Sprite PlantSprite2;
+    public Sprite PlantSprite3;
+    public Sprite PlantSprite4;
+    public Sprite PlantSprite5;
+
+    public GameObject WaterMeImg;
+
+    // Use this for initialization
+    void Start ()
+    {
+        PlantSprite = this.gameObject.GetComponent<Image>();
+        setupPlant();
+    }
+	
+    public void setupPlant()
+    {
+        PlantSprite.sprite = PlantSprite1;
+
+        PlantGrowth = 0;
+        WaterPercentage = 100;
+        WaterMeImg.SetActive(false);
+        PlantDone = false;
+
+        StartCoroutine(plantGrow());
+    }
+
+    IEnumerator plantGrow()//grows plant over time and drains water
+    {
+        while (PlantDone == false)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (WaterPercentage > 0)
+            {
+                PlantGrowth += 5;
+                WaterPercentage -= 10;
+
+                plantSpriteCheck();
+            }else WaterMeImg.SetActive(true);
+
+            if (PlantGrowth >= 100)
+            {
+                PlantDone = true;//ends coroutine loop
+            }
+        }
+    }
+
+    public void plantSpriteCheck()//check which stage the plant is in and change sprite if needed
+    {
+        if(PlantGrowth >= 0 && PlantGrowth < 25)
+        {
+            PlantSprite.sprite = PlantSprite1;
+        }
+        if (PlantGrowth >= 25 && PlantGrowth < 50)
+        {
+            PlantSprite.sprite = PlantSprite2;
+        }
+        if (PlantGrowth >= 50 && PlantGrowth < 75)
+        {
+            PlantSprite.sprite = PlantSprite3;
+        }
+        if (PlantGrowth >= 75 && PlantGrowth < 100)
+        {
+            PlantSprite.sprite = PlantSprite4;
+        }
+        if(PlantGrowth >= 100)
+        {
+            PlantSprite.sprite = PlantSprite5;
+        }
+    }
+
+    public void plantClicked()
+    {
+        if (PlantDone)//collect coins
+        {
+            setupPlant();
+        }
+        else waterPlant();
+    }
+
+    public void waterPlant()
+    {
+        WaterPercentage = 100;
+        WaterMeImg.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIControler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControler : MonoBehaviour {
+
+    public GameObject ShopPanel;
+    public GameObject TreePanel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        closeAll();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            closeAll();
+        }
+    }
+
+    public void closeAll()
+    {
+        ShopPanel.SetActive(false);
+        TreePanel.SetActive(false);
+    }
+
+    public void toggleShopPanel()
+    {
+        if(ShopPanel.activeSelf)
+            ShopPanel.SetActive(false);
+        else ShopPanel.SetActive(true);
+    }
+
+    public void toggleTreePanel()
+    {
+        if (TreePanel.activeSelf)
+            TreePanel.SetActive(false);
+        else TreePanel.SetActive(true);
+    }
+}
